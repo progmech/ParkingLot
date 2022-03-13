371996c2497bd207f75dc6d3139a577d165b31ee
Andrei Astakhov <andrei.astakhov@hotmail.com>
2022-03-13 15:59:13 +0300

    Stage 1/5: First steps

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
     println("White car has parked.")
     println("Yellow car left the parking lot.")
-    println("Green car just parked here.}")
+    println("Green car just parked here.")
 }
