a048a238fa9bba47c0f705057e8142d4f4b05d95
Andrei Astakhov <andrei.astakhov@hotmail.com>
2022-03-14 14:12:08 +0300

    Stage 3/5: Expand and park

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,6 @@
 package parking
 
 fun main() {
-//    val parkingLot = mutableListOf(1)
-//    val commandArgs = readln().split(" ")
-//    if (commandArgs.first() == "park") {
-//        println("${commandArgs.last()} car parked in spot 2.")
-//    } else {
-//        val spot = commandArgs[1]
-//        if (parkingLot.contains(spot.toInt())) {
-//            parkingLot.remove(spot.toInt())
-//            println("Spot $spot is free.")
-//        } else {
-//            println("There is no car in spot $spot.")
-//        }
-//    }
-
     val parkingLot = IntArray(20) { 0 }
 
     do {
