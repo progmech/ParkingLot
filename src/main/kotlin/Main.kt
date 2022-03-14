aed250a3e7ef070e691d30f80e14bf797f42484c
Andrei Astakhov <andrei.astakhov@hotmail.com>
2022-03-14 15:10:43 +0300

    Stage 4/5: Size matters

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,62 @@
 package parking
 
+data class Car(val number: String, val color: String)
+
 fun main() {
-    val parkingLot = IntArray(20) { 0 }
+    var parkingLotSize: Int
+    var parkingLot: Array<Car?> = emptyArray()
 
     do {
         val inputArgs = readln().split(" ")
         val command = inputArgs.first()
-    if (command == "park") {
-        val freeSpotIndex = parkingLot.indexOfFirst { it == 0 }
-        if (freeSpotIndex == -1) {
-            println("Sorry, the parking lot is full.")
-            continue
+        when (command) {
+            "create" -> {
+                parkingLotSize = inputArgs.last().toInt()
+                parkingLot = createParkingLot(parkingLotSize)
+            }
+            "park" -> {
+                if (parkingLot.isEmpty()) {
+                    println("Sorry, a parking lot has not been created.")
+                    continue
+                }
+                val freeSpotIndex = parkingLot.indexOfFirst { it == null }
+                if (freeSpotIndex == -1) {
+                    println("Sorry, the parking lot is full.")
+                    continue
+                }
+                val car = Car(inputArgs[1], inputArgs.last())
+                parkingLot[freeSpotIndex] = car
+                println("${car.color} car parked in spot ${freeSpotIndex + 1}.")
+            }
+            "status" -> {
+                if (parkingLot.isEmpty()) {
+                    println("Sorry, a parking lot has not been created.")
+                    continue
+                }
+                if (parkingLot.filterNotNull().isEmpty()) {
+                    println("Parking lot is empty.")
+                    continue
+                }
+                for (car in parkingLot.filterNotNull()) {
+                    println("${parkingLot.indexOf(car) + 1} ${car.number} ${car.color}")
+                }
+            }
+            "leave" -> {
+                if (parkingLot.isEmpty()) {
+                    println("Sorry, a parking lot has not been created.")
+                    continue
+                }
+                val spot = inputArgs.last().toInt()
+                parkingLot[spot - 1] = null
+                println("Spot $spot is free.")
+            }
         }
-        parkingLot[freeSpotIndex] = 1
-        println("${inputArgs.last()} car parked in spot ${freeSpotIndex + 1}.")
-    } else if (command == "leave") {
-        val spot = inputArgs.last().toInt()
-        parkingLot[spot - 1] = 0
-        println("Spot $spot is free.")
-    }
-
     } while (command != "exit")
 }
+
+fun createParkingLot(parkingLotSize: Int): Array<Car?> {
+    println("Created a parking lot with $parkingLotSize spots.")
+    return arrayOfNulls(parkingLotSize)
+}
+
+
