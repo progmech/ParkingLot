c0069abcfd2e66126a70b0ed102f44a567471761
Andrei Astakhov <andrei.astakhov@hotmail.com>
2022-03-14 08:22:07 +0300

    Stage 2/5: Only 2 spots

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
+package parking
+
 fun main() {
-    println("White car has parked.")
-    println("Yellow car left the parking lot.")
-    println("Green car just parked here.")
+    val parkingLot = mutableListOf(1)
+    val commandArgs = readln().split(" ")
+    if (commandArgs.first() == "park") {
+        println("${commandArgs.last()} car parked in spot 2.")
+    } else {
+        val spot = commandArgs[1]
+        if (parkingLot.contains(spot.toInt())) {
+            parkingLot.remove(spot.toInt())
+            println("Spot $spot is free.")
+        } else {
+            println("There is no car in spot $spot.")
+        }
+    }
 }
